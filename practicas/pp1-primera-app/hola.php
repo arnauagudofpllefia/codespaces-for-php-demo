e540927f0c2f4652e89993e91f5530ad6dbd59e9
arnauagudofpllefia <arnagudo05@gmail.com>
2025-09-15 16:30:40 +0000

    hola.php7

--- practicas/pp1-primera-app/hola.php
+++ practicas/pp1-primera-app/hola.php
@@ -4,24 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="estilo.css">
+  
 </head>
 <body>
-     <body>
-      <head>
-        <img src="logo-fpllefia.jpg" alt="logo">
+     
+      <header>
+        <img src="logo-fpllefia.png" alt="logo">
         <h1><?php echo "Módulo 7 - Práctica 1. Mi primera aplicación en PHP";?> </h1>
-      </head>
+      </header>
 
-      <div>
-        <img src="fotomia.png" alt="fotomia">
+      <div class="columnas">
+        <div class="columna1">
+          <img src="fotomia.png" alt="fotomia">
+          <p><?php echo "Arnau Agudo Torello";?></p>
+        </div>
+        <div class="columna2">
+          <p><?php echo "php: Es  para que el servidor detecte que las siguientes lineas van a ser de php. Function: Es una funcion llamada sayHello que recibe un nombre y devuelve un texto con el nombre que ha recibido. SayHello('remote world'): llama a la funcion sayHello y le envia un nombre en este caso world. Phpinfo();: llama a la funcion phpinfo";?></p>
+        </div>
+
+      </div>
+
+      <footer>
         <p><?php echo "Arnau Agudo Torello";?></p>
-      </div>
-      <div>
-        <p><?php echo "<?php: Es  para que el servidor detecte que las siguientes lineas van a ser de php. Function: Es una funcion llamada sayHello que recibe un nombre y devuelve un texto con el nombre que ha recibido. SayHello('remote world'): llama a la funcion sayHello y le envia un nombre en este caso world. Phpinfo();: llama a la funcion phpinfo";?></p>
-      </div>
+        <p><?php echo "15/09/2025";?></p>
+      </footer>
+      
         
         
         
-    </body>
+    
 </body>
 </html>
