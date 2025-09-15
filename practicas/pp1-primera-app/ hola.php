977f423f92964b905b3f7d32215aec3ebf2a611c
arnauagudofpllefia <arnagudo05@gmail.com>
2025-09-15 14:12:46 +0000

    hola.php5

--- practicas/pp1-primera-app/ hola.php
+++ practicas/pp1-primera-app/ hola.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <html>
     <head>
-        
+        <img src="logo-fpllefia.jpg" alt="logo">
         <h1><?php echo "Módulo 7 - Práctica 1. Mi primera aplicación en PHP";?> </h1>
     </head>
     <body>
