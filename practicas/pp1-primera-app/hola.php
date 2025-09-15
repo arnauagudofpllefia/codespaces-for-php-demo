4b824d13c523ca2d91596b459c5572d100a43525
arnauagudofpllefia <arnagudo05@gmail.com>
2025-09-15 14:52:07 +0000

    hola,php6

---
+++ practicas/pp1-primera-app/hola.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <body>
+      <head>
+        <img src="logo-fpllefia.jpg" alt="logo">
+        <h1><?php echo "Módulo 7 - Práctica 1. Mi primera aplicación en PHP";?> </h1>
+      </head>
+
+      <div>
+        <img src="fotomia.png" alt="fotomia">
+        <p><?php echo "Arnau Agudo Torello";?></p>
+      </div>
+      <div>
+        <p><?php echo "<?php: Es  para que el servidor detecte que las siguientes lineas van a ser de php. Function: Es una funcion llamada sayHello que recibe un nombre y devuelve un texto con el nombre que ha recibido. SayHello('remote world'): llama a la funcion sayHello y le envia un nombre en este caso world. Phpinfo();: llama a la funcion phpinfo";?></p>
+      </div>
+        
+        
+        
+    </body>
+</body>
+</html>
