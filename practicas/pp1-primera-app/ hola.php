ee93c9731184fcbe15943b365e3f985a5b2ee769
arnauagudofpllefia <arnagudo05@gmail.com>
2025-09-15 14:02:23 +0000

    hola.php4

--- practicas/pp1-primera-app/ hola.php
+++ practicas/pp1-primera-app/ hola.php
@@ -1,17 +1,9 @@
-<?php
-
-function sayHello($name) {
-	echo "Hello $name!";
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 <html>
     <head>
-        <img src="../pp1-primera-app/logo-fpllefia.jpg" alt="logo">
-        <h1> Módulo 7 - Práctica 1. Mi primera aplicación en PHP</h1>
+        
+        <h1><?php echo "Módulo 7 - Práctica 1. Mi primera aplicación en PHP";?> </h1>
     </head>
     <body>
         
