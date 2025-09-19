a4d8f761213c04cb61e12eb159613af9a42c1067
arnauagudofpllefia <arnagudo05@gmail.com>
2025-09-19 17:30:25 +0000

    hola8

---
+++ practicas/pp2-EstructurasDeControl/ex1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Nombres parells entre 50 i 500</h1>
+    <?php
+    for($i=0;;#i++){
+    echo "<div></div>"
+    };
+
+    ?>
+</body>
+</html>
